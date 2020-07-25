29e934182df0428e0b921d52450808b124bea6c1
Rajashow <rajashowparajuli@gmail.com>
2020-07-25 14:23:58 -0400

    add audio api processing

--- src/django_server/views.py
+++ src/django_server/views.py
@@ -5,9 +5,51 @@ from urllib.parse import unquote
 from .third_party.get_mask_predict import get_prediction
 import re
 
-DATA_URL_PATTERN = re.compile("data=data:image/(png|jpeg);base64,(.*)$")
-MODEL_ID = "ICN3966723492690264064"
-PROJECT_ID = "767032446048"
+# from google.cloud import speech
+
+# DATA_URL_PATTERN = re.compile("data=data:image/(png|jpeg);base64,(.*)$")
+# MODEL_ID = "ICN3966723492690264064"
+# PROJECT_ID = "767032446048"
+
+# # Audio recording parameters
+# STREAMING_LIMIT = 240000 * 5  # 20 minutes
+# SAMPLE_RATE = 16000
+# CHUNK_SIZE = int(SAMPLE_RATE / 10)  # 100ms
+
+# RED = "\033[0;31m"
+# GREEN = "\033[0;32m"
+# YELLOW = "\033[0;33m"
+
+# client = speech.SpeechClient()
+# config = speech.types.RecognitionConfig(
+#     encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+#     sample_rate_hertz=SAMPLE_RATE,
+#     language_code="en-US",
+#     max_alternatives=1,
+# )
+# streaming_config = speech.types.StreamingRecognitionConfig(
+#     config=config, interim_results=True
+# )
+
+
+# def something_request(request):
+#     audio_generator = []
+#     requests = (
+#         speech.types.StreamingRecognizeRequest(audio_content=content)
+#         for content in audio_generator
+#     )
+
+#     responses = client.streaming_recognize(streaming_config, requests)
+#     for response in responses:
+#         if not response.results:
+#             continue
+
+#         result = response.results[0]
+
+#         if not result.alternatives:
+#             continue
+
+#         transcript = result.alternatives[0].transcript
 
 
 @csrf_exempt
