f260a872bdfb21b920f73b9c7966a8dbc5567974
Rajashow <rajashowparajuli@gmail.com>
2020-07-25 12:59:12 -0400

    preds for mask is returned

--- src/django_server/views.py
+++ src/django_server/views.py
@@ -5,7 +5,7 @@ from urllib.parse import unquote
 from .third_party.get_mask_predict import get_prediction
 import re
 
-DATA_URL_PATTERN = re.compile('data=data:image/(png|jpeg);base64,(.*)$')
+DATA_URL_PATTERN = re.compile("data=data:image/(png|jpeg);base64,(.*)$")
 MODEL_ID = "ICN3966723492690264064"
 PROJECT_ID = "767032446048"
 
@@ -16,8 +16,9 @@ def test(request):
         if request.is_ajax():
             image_data = unquote(request.body.decode())
             # image_data = DATAURLPATTERN.match().group(2)
-            image_data = image_data[27:].encode()
-            # preds = get_prediction(image_data, PROJECT_ID, MODEL_ID)
-            image_data = base64.b64decode(image_data)
+            image_data_trim = image_data[27:].encode()
+            image_data_trim = base64.b64decode(image_data_trim)
+            preds = get_prediction(image_data_trim, PROJECT_ID, MODEL_ID)
+            print(preds)
 
-    return render(request, 'camera.html', {})
+    return render(request, "camera.html", {})
